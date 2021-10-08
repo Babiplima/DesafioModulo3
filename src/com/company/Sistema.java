7bc51f35e0c95237c5a668b7be86c7104c6e8506
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-08 14:30:12 -0300

    Criar menu sistema classe sistema

--- src/com/company/Sistema.java
+++ src/com/company/Sistema.java
@@ -11,4 +11,17 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+
+    public static void menu(){
+        System.out.println("Bem vindos É do Bem Comidas Saudáveis ");
+        System.out.println("Digite 1 - para cadastrar um cliente. ");
+        System.out.println("Digite 2 - para cadastrar um vendedor. ");
+        System.out.println("Digite 3 - para registrar uma venda. ");
+        System.out.println("Digite 4 - para listar clientes. ");
+        System.out.println("Digite 5 - para listar vendedores. ");
+        System.out.println("Digite 6 - para apresentar as vendas registradas. ");
+        System.out.println("Digite 7 - para sair. ");
+    }
+
+
 }
