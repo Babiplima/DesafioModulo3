c3a8a02a1abb65cc70b0f2f1c3ddeb324336fadc
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-08 11:22:49 -0300

    Modelar classe clientes

--- src/com/company/Clientes.java
+++ src/com/company/Clientes.java
@@ -1,4 +1,64 @@
 package com.company;
 
 public class Clientes {
+
+    //Atributos
+    private String nome;
+    private String cpf;
+    private String email;
+
+    //Método construtores
+
+
+    public Clientes() {
+    }
+
+    public Clientes(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    //Permissões para manipular atributos Get e Set
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //Método toString
+
+    @Override
+    public String toString() {
+        StringBuilder exibir = new StringBuilder();
+        exibir.append(super.toString());
+        exibir.append("\n =============================== ");
+        exibir.append("\n Nome " + nome);
+        exibir.append(("\n E-mail " + email));
+        exibir.append(("\n E-mail " + cpf));
+        exibir.append("\n =============================== ");
+        return exibir.toString();
+    }
 }
+
+
